b02d975a15048b1acfc039a863ab6fdd15e5e19d
Radek Koubsky <radekkoubsky@gmail.com>
2016-01-12 23:44:57 +0100

    Typo.

--- src/main/java/org/jboss/byteman/koubsky/servlets/ServletHandlerLog.java
+++ src/main/java/org/jboss/byteman/koubsky/servlets/ServletHandlerLog.java
@@ -23,7 +23,7 @@ public class ServletHandlerLog extends Helper{
         final Logger logger = LogManager.getLogManager().getLogger(clazz);
         if(logger != null){
             final Level level = Level.parse("DEBUG");
-            logger.log(level, "Request coockies:");
+            logger.log(level, "Request cookies:");
             final Map<String, Cookie> cookies = exchange.getRequestCookies();
             final Iterator<String> it = cookies.keySet().iterator();
             while(it.hasNext()) {
